46d89f48a616b3f12466a679cc68891aa5eaad18
Syahran <kosong0@pop-os.localdomain>
2021-10-11 12:16:11 +0800

    insert image

--- database.sql
+++ database.sql
@@ -1,9 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 11 Okt 2021 pada 05.37
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
@@ -13,19 +35,18 @@ CREATE TABLE `admin` (
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data untuk tabel `admin`
+--
 
 INSERT INTO `admin` (`id`, `username`, `password`, `email`, `tanggal`) VALUES
 (1, 'Admin', '$2y$10$FfhPYubR4sOXAFSd3NzyQ.C77L4.qIsCa/YlYZCn.2eK8rfWr6oiq', 'admin@email.com', '2021-09-19 14:39:53');
 
+-- --------------------------------------------------------
 
-
-CREATE TABLE `page` (
-  `id` int(11) NOT NULL,
-  `page_name` varchar(255) DEFAULT NULL,
-  `konten` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
+--
+-- Struktur dari tabel `artikel`
+--
 
 CREATE TABLE `artikel` (
   `id` int(11) NOT NULL,
@@ -35,3 +56,39 @@ CREATE TABLE `artikel` (
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `page`
+--
+
+CREATE TABLE `page` (
+  `id` int(11) NOT NULL,
+  `page_name` varchar(255) DEFAULT NULL,
+  `konten` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- new.php
+++ new.php
@@ -12,23 +12,45 @@ if (isset($_POST['submit'])) {
     $tanggal = date('Y-m-d H:i');
     $penulis = mysqli_real_escape_string($dbcon, $_SESSION['username']);
 
-    $sql = "INSERT INTO artikel (judul, diskripsi, penulis, tanggal) VALUES('$judul', '$diskripsi', '$penulis', '$tanggal')";
+    $upload = "upload/";
+    $rand   = time();
+    $gambar      = str_replace(' ','-',strtolower($_FILES['image']['name'][0]));
+    $ImageType      = $_FILES['image']['type'][0];
+ 
+    $gmbrExt = substr($gambar, strrpos($gambar, '.'));
+    $gmbrExt       = str_replace('.','',$gmbrExt);
+    $gambar      = preg_replace("/\.[^.\s]{3,4}$/", "", $gambar);
+    $namaGambar = $gambar.'-'.$rand.'.'.$gmbrExt;
+    $ret[$namaGambar]= $upload.$namaGambar;
+
+    
+    if (!file_exists($upload))
+    {
+        @mkdir($upload, 0777);
+    }               
+    move_uploaded_file($_FILES["image"]["tmp_name"][0],$upload."/".$namaGambar );
+
+
+
+    $sql = "INSERT INTO artikel (judul, diskripsi, gambar, penulis, tanggal) VALUES('$judul', '$diskripsi', '$namaGambar', '$penulis', '$tanggal')";
     mysqli_query($dbcon, $sql) or die("gagal membuat artikel" . mysqli_connect_error());
 
-    printf("Artikel sudah di publikasikan. <meta http-equiv='refresh' content='2; url=view.php?id=%d'/>",
+    printf("Artikel sudah di publikasikan.",
         mysqli_insert_id($dbcon));
 
 
 } else {
 
     echo '
-<form class="w3-container" method="POST">
+<form class="w3-container" method="POST" enctype="multipart/form-data">
 <label>Judul</label>
 
 <input type="text" class="w3-input w3-border" name="title" required>
 <br>
 
-<label>Arikel</label>
+<input type="file" class="w3-input w3-border" name="image[]" required/>
+<br>
+
 <textarea id="description" row="30" cols="50" class="w3-input w3-border" name="description" required/></textarea>
 <br>
 
